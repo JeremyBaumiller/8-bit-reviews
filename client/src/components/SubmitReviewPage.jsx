c3df348c70a4b63ae8a3bc9e3bc751b174f7d754
JeremyBaumiller <jbaumil07@gmail.com>
2024-06-23 20:24:35 -0400

    removed multiple routers and fixed homepage

---
+++ client/src/components/SubmitReviewPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SubmitReviewPage = () => {
+  return (
+    <div>
+      <h1>Submit a Review</h1>
+    </div>
+  );
+};
+export default SubmitReviewPage;
---
+++ client/src/components/UserProfilePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const UserProfilePage = () => (
+  <div>
+    <h1>User Profile</h1>
+  </div>
+);
+
+export default UserProfilePage;
