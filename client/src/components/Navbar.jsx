e3d59b4b4fe6e33c06483d19bfb04bdc1efc0231
JeremyBaumiller <jbaumil07@gmail.com>
2024-06-26 17:30:17 -0400

    logo added to nave bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,12 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => (
   <nav>
-    <img
-      src={
-        "/home/jam_master_jerm/Coursework/8-bit-reviews/client/src/assets/8-bit-reviews-high-resolution-logo-white.png"
-      }
-      alt="My Image"
-    />
+    <img src="covers/logo.png" className="logo" alt="" />
     <Link to="/">Home</Link>
     <Link to="/games">Games</Link>
     <Link to="/profile">Profile</Link>
--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const { client, createGame, createTable, fetchGames } = require("./db2");
-
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+const { options } = require("pg/lib/defaults");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "public")));
 
 // Connect to the database and initialize tables
 const init = async () => {
